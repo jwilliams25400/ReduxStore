64d47bbe8e98c323851312434b8f8f39b46907d9
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-14 18:48:57 -0400

    store.js and path

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import store from "./utils/store";
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -14,9 +15,11 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import { Provider } from 'react-redux';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -42,7 +45,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store = {store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +56,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
